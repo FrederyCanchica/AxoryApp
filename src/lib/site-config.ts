d7a2482242311aecefbd5d634331ac2909710d6e
Fredery Canchica <frederycanchicaspain@gmail.com>
2026-05-20 17:33:52 +0200

    Integration of whatsapp number

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -2,7 +2,7 @@
 export const siteConfig = {
   brand: "aXory",
   tagline: "Sistemas web que trabajan por ti",
-  whatsappNumber: "34600000000", // Cambia este número por el real
+  whatsappNumber: "34687641716", // Cambia este número por el real
   whatsappPrefilled: "Hola, vengo desde la web de aXory. Me gustaría más información.",
   calendarUrl: "https://cal.com/axory/30min", // Cambia por tu Calendly/Cal.com
   email: "hola@axory.com",
